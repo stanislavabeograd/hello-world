c588f4827029482cdc0236604b5e60172efa1625
Stanislava Gardasevic <stanislavabeograd@gmail.com>
2021-03-12 16:09:25 -1000

    a

---
+++ Lab11/Ex1.js
@@ -0,0 +1,11 @@
+attributes  =  "Stasa; 37.5; -36.5; CIS";
+var i;
+var x ="";
+pieces = attributes.split (";");
+    for (i=0; i<pieces.length; i++)
+        {x += " " + typeof(pieces[i]) + "," + pieces[i] + ",";
+        }
+
+console.log(x);
+
+
