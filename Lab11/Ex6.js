0ddd141feb35cc8d6f9297490276cd61f66daaa3
stasha-g <stanislavabeograd@gmail.com>
2021-03-17 22:39:57 -1000

    a

---
+++ Lab11/Ex6.js
@@ -0,0 +1,24 @@
+
+attributes  =  "Stasa; 37; -36.5; CIS";
+pieces = attributes.split (";");
+
+function isNonNegInt(q, returnErrors = false) //this function checks whether the item in array is a whole number or not.
+{
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors : (errors.length == 0);
+}
+
+for (i=0; i<= attributes.length; i++){
+    console.log(pieces);
+}
+   
+{
+function checkIt() { 
+    console.log(`part ${i} is ${(isNonNegInt(pieces[i])?'a':'not a')} quantity`);
+}
+}
+
+pieces.forEach(checkIt);
