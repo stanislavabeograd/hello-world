7ac69b9fa71971b82b89e0650893c0ece25ef991
stasha-g <stanislavabeograd@gmail.com>
2021-03-21 22:40:31 -1000

    a

--- Assignment1/professors.js
+++ Assignment1/professors.js
@@ -1,10 +1,10 @@
 professors = [
-    { "name": "Irvin, Vanessa", "affiliation": "Assistant Professor", "department": "LIS", "research_area": "Public libraries, reference services, youth services, social media" },
-    { "name": "Sadowski, Peter", "affiliation": "Assistant Professor", "department": "ICS", "research_area": "Machine learning. artificial intelligence, neural systems" },
-    { "name": "Gazan, Rich", "affiliation": "Professor", "department": "LIS", "research_area": "Interdisciplinary scientific collaborations, social Q&A commnunities, information sharing" },
-    { "name": "Bergstrom, Kelly", "affiliation": "Assistant Professor", "department": "COM", "research_area": "Digital Cultures, Game Studies, Feminist Methods" },
-    { "name": "Port, Daniel", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Information Technology Engineering; Methods in Software Engineering; IT Education" },
-    { "name": "Xiao, Bo", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Human-Computer Interaction, Dark Side of Digitization, Health Information Systems" }
+    { "name": "Irvin, Vanessa", "affiliation": "Assistant Professor", "department": "LIS", "research_area": "Public libraries, reference services, youth services, social media" , "image" : "../images/Vanessa.jpg" },
+    { "name": "Sadowski, Peter", "affiliation": "Assistant Professor", "department": "ICS", "research_area": "Machine learning. artificial intelligence, neural systems" , "image" : "../images/Peter.jpg" },
+    { "name": "Gazan, Rich", "affiliation": "Professor", "department": "LIS", "research_area": "Interdisciplinary scientific collaborations, social Q&A commnunities, information sharing" , "image" : "../images/Rich.png" },
+    { "name": "Bergstrom, Kelly", "affiliation": "Assistant Professor", "department": "COM", "research_area": "Digital Cultures, Game Studies, Feminist Methods" , "image" : "../images/kelly.png" },
+    { "name": "Port, Daniel", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Information Technology Engineering; Methods in Software Engineering; IT Education" , "image" : "../images/Daniel.jpg" },
+    { "name": "Xiao, Bo", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Human-Computer Interaction, Dark Side of Digitization, Health Information Systems" , "image" : "../images/Bo.jpg" }
 ];
 
 console.log("data loded.")
