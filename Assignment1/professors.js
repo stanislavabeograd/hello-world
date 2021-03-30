39c391a3669e7a33a0c53ba551b0ad57ccb055fb
Stanislava Gardasevic <stanislavabeograd@gmail.com>
2021-03-30 13:57:44 -1000

    1

--- Assignment1/professors.js
+++ Assignment1/professors.js
@@ -1,10 +1,10 @@
 professors = [
-    { "name": "Irvin, Vanessa", "affiliation": "Assistant Professor", "department": "LIS", "research_area": "Public libraries, Reference services, Youth services, Social media" , "image" : "../images/Vanessa.jpg" },
-    { "name": "Sadowski, Peter", "affiliation": "Assistant Professor", "department": "ICS", "research_area": "Machine learning, Artificial intelligence, Deeep learning with neural networks" , "image" : "../images/Peter.jpg" },
-    { "name": "Gazan, Rich", "affiliation": "Professor", "department": "LIS", "research_area": "Interdisciplinary scientific collaborations, Social Q&A commnunities, Information sharing" , "image" : "../images/Rich.png" },
-    { "name": "Bergstrom, Kelly", "affiliation": "Assistant Professor", "department": "COM", "research_area": "Digital Cultures, Game Studies, Feminist Methods" , "image" : "../images/kelly.png" },
-    { "name": "Port, Daniel", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Information Technology Engineering, Methods in software engineering, IT Education" , "image" : "../images/Daniel.jpg" },
-    { "name": "Xiao, Bo", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Human-Computer Interaction, Dark side of digitization, Health information systems" , "image" : "../images/Bo.jpg" }
+    { "name": "Irvin, Vanessa", "affiliation": "Assistant Professor", "department": "LIS", "research_area": "Public libraries, Reference services, Youth services, Social media" , "image" : "./images/Vanessa.jpg" },
+    { "name": "Sadowski, Peter", "affiliation": "Assistant Professor", "department": "ICS", "research_area": "Machine learning, Artificial intelligence, Deeep learning with neural networks" , "image" : "./images/Peter.jpg" },
+    { "name": "Gazan, Rich", "affiliation": "Professor", "department": "LIS", "research_area": "Interdisciplinary scientific collaborations, Social Q&A commnunities, Information sharing" , "image" : "./images/Rich.png" },
+    { "name": "Bergstrom, Kelly", "affiliation": "Assistant Professor", "department": "COM", "research_area": "Digital Cultures, Game Studies, Feminist Methods" , "image" : "./images/kelly.png" },
+    { "name": "Port, Daniel", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Information Technology Engineering, Methods in software engineering, IT Education" , "image" : "./images/Daniel.jpg" },
+    { "name": "Xiao, Bo", "affiliation": "Associate Professor", "department": "ITM", "research_area": "Human-Computer Interaction, Dark side of digitization, Health information systems" , "image" : "./images/Bo.jpg" }
 ];
 
 console.log("data loded.")
