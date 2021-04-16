5fda0f1ddb8f7941afb91f77b6724ccce9e22332
Stanislava Gardasevic <stanislavabeograd@gmail.com>
2021-04-15 16:29:06 -1000

    a

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -4,26 +4,65 @@ var myParser = require("body-parser"); //takes a body of the request and creates
 const { response } = require('express');
 app.use(myParser.urlencoded({ extended: true })); //need to add this
 var qs = require('qs');
+var fs = require('fs'); //loading file system
 
+
+
+//here I am reading in the user data into an object called user_data
+var user_data_file = './user_data.json';
+var file_stats = fs.statSync(user_data_file);
+var user_data = JSON.parse(fs.readFileSync('./user_data.json', 'utf-8')); //gonna read it all as a string in this var.
 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path + 'with query' + JSON.stringify(request.query));
     next(); //passing it on to the next who can respond
-}); // this is diagnostic, to see what's the metod, query etc.
+}); // this is diagnostic, to see what's the metod, query etc
+//this code is processing the registration form and input into the file with all the user data
 
-app.get('/test', function (request, response, next) {
-    response.send('I got a request for /test'); 
-    }); // if I get get request, then execute this function
+app.post('/process_register', function (req, res) {
+    username = req.body.uname;
+    // adding new user to the object
+    if (typeof user_data[username] == 'undefined') {  
+        user_data[username] = {}; // sets up space for the newuser (empty object)
+        user_data[username].password = req.body["psw"]; // alternative reg_data[username].["password"] = 'newpass';
+        user_data[username].email = req.body["email"];
+        user_data[username].name = req.body["name"];
+            //checking if the email is already taken
+    if (typeof user_data[username].email != 'undefined') {
+        res.send(`It seems that you are already registered with the following email: ${user_data[username].email}, try <a href ="./login.html">logging in.</a>`);
+}
+        //convert the updated userdata object to json and write it to the file
+        fs.writeFileSync('./user_data.json', JSON.stringify(user_data));
+        res.redirect('invoice.html?' + qs.stringify(req.query));
+    } else {
+        res.send(`${username} is taken`);
+    }
+});
+
+
+//this part is processing the login page
+app.post('/process_login', function (request, response, next) {
+    console.log(request.body); //getting the body stuff
+    let username_entered = request.body.uname;  //making an object from the input form for username
+    let password_entered = request.body.psw; //making an object from the input form for password
+    request.query["uname"] = request.body["uname"]; //adding username to the URL of invoice
+    if (typeof user_data[username_entered] != 'undefined') { //if the entered username undefined, that means I have it in my data already, so they can proceede to invoice
+        if (user_data[username_entered]['password'] == password_entered) {
+            response.redirect('invoice.html?' + qs.stringify(request.query));
+        }
+        else {
+            response.send(`${username_entered}, your password is not correct, please re-enter.`);// if the username is not recognized, it sends the message to go back to the registration form
+        }
+    }
+    if (typeof user_data[username_entered] == 'undefined') {// checking if username is there
+        response.send(`<h2>${username_entered}</h2> username is not recognized. <br><br> Please go to the <a href="./registration.html"> Registration form </a> to create a profile`);
+
+    }
+}); 
+
 
 
 /*
-//check if the server has access to your form
-app.post('/process_login', function (request, response, next) { //ovo odgovara na POST u method u html form
-    post_data = request.body; //gets the data form the post in the variable    
-    response.send(post_data);
-});
-*/
-
 // processing login
 app.post('/process_login', function (request, response, next) { 
     console.log(request.query);
@@ -39,7 +78,7 @@ app.post('/', function (request, response, next) {
     response.send(request.body);
 });
 
- /*
+ 
     user_data = {'uname':'stasa', 'password' : 'car'};
     post_data = request.body; //gets the data form the post in the variable    
     if(post_data['uname']){  //check the username if there is some data
--- Lab14/server_14.js
+++ Lab14/server_14.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-var myParser = require("body-parser"); //takes a body of the request and creates an object of the data in it (POST data)
+var myParser = require("body-parser"); //takes a body of the request -data- and creates an object of the data in it (POST data)
 const { response } = require('express');
-app.use(myParser.urlencoded({ extended: true })); //need to add this
+app.use(myParser.urlencoded({ extended: true })); //need to add this to get the data from the post
 var qs = require('qs');
 var fs = require('fs'); //loading file system
 const e = require('express');
@@ -12,19 +12,42 @@ const e = require('express');
 //console.log(user_data['stasa']['password']);
 
 var user_data_file = './user_data.json';
-if (fs.existsSync(user_data_file)){
+if (fs.existsSync(user_data_file)) {
     var file_stats = fs.statSync(user_data_file);
- //   console.log(file_stats["size"]);
+    //   console.log(file_stats["size"]);
     var user_data = JSON.parse(fs.readFileSync('./user_data.json', 'utf-8')); //gonna read it all as a string in this var.
 } else {
-  //  console.log(`${user_data_file} does not exist!`);
+    //  console.log(`${user_data_file} does not exist!`);
 }
+
 
 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to path ' + request.path + 'with query' + JSON.stringify(request.query));
     next(); //passing it on to the next who can respond
 }); // this is diagnostic, to see what's the metod, query etc.
+
+//u assignment 2 mora da proveris da li postoji u fajlu vec -- ako je undefind znaci postoji, onda pass da li ima dovoljno karaktera, onda da li se matchuju pass sa reentered pass 
+
+
+
+//function to read the data from the register form and write it in the file
+app.post('/process_register', function (req, res) {
+    username = req.body.username;
+    // adding new user to the object
+    if (typeof user_data[username] == 'undefined') {  
+        user_data[username] = {}; // sets up space for the newuser (empty object)
+        user_data[username].password = req.body["password"]; // alternative reg_data[username].["password"] = 'newpass';
+        user_data[username].email = req.body["email"];
+        user_data[username].first_name = req.body["first_name"];
+        //convert the updated userdata object to json and write it to the file
+        fs.writeFileSync('./user_data.json', JSON.stringify(user_data));
+        res.send(`${username} is registered`);
+    } else {
+        res.send(`${username} is taken`);
+    }
+}
+)
 
 app.get('/test', function (request, response, next) {
     response.send('I got a request for /test');
@@ -40,24 +63,9 @@ app.post('/process_login', function (request, response, next) { //ovo odgovara n
 */
 
 
-app.post('/process_login', function (request, response, next) { //ovo odgovara na POST u method u html form
-    console.log(request.body);
-    let username_entered = request.body.username;
-    let password_entered = request.body.password;
-    if(typeof user_data[username_entered] != 'undefined') { //ako nije undefined onda ga imam u data
-      if(user_data[username_entered]['password'] == password_entered){
-          response.send(`${username_entered} is logged in.`)
-      }
-      else {
-        response.send(`${username_entered}'s password is wrong.`)
 
-      }
-    }
-    if(typeof user_data[username_entered] == 'undefined'){
-        response.send(`${username_entered} is not recognized.`)
 
-    } 
-}); //this responds to post request (u ovom slucaju process_login) i pise u body 
+
 
 
 app.use(express.static('./public')); //get request goes here to look for file
